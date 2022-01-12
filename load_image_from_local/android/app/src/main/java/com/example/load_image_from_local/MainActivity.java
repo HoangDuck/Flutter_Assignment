e7a80620764cdee9b906d7fe6f3e72eb22075af4
HoangDuck <hoanghuuduc22@gmail.com>
2022-01-12 16:51:20 +0700

    Commit clear cache image

---
+++ load_image_from_local/android/app/src/main/java/com/example/load_image_from_local/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.load_image_from_local;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ load_image_from_local/lib/main.dart
@@ -0,0 +1,34 @@
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.file(File("/data/user/0/com.example.social_media/cache/image_picker8337059850139395467.jpg")),
+    );
+  }
+}
