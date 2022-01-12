86651cde5eb7717311ce4b413c480c95bc77bed2
HoangDuck <hoanghuuduc22@gmail.com>
2022-01-12 13:54:00 +0700

    commit upload images

--- social_media/lib/view/uploadstatus.dart
+++ social_media/lib/view/uploadstatus.dart
@@ -9,12 +9,14 @@ class UploadStatus extends StatefulWidget {
 }
 
 class _UploadStatusState extends State<UploadStatus> {
+
   @override
   Widget build(BuildContext context) {
     DataConvert dataConvert=Provider.of<DataConvert>(context);
     User user=dataConvert.currentUser;
     return Material(
       child: Container(
+        color: Colors.blueAccent,
         padding: const EdgeInsets.all(10),
         child: Container(
           decoration: const ShapeDecoration(
@@ -34,53 +36,69 @@ class _UploadStatusState extends State<UploadStatus> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                height: 65.0,
-                child: Row(
-                  children: [
-                    const SizedBox(width: 10,),
-                    SizedBox(
-                      height: 55,
-                      width: 55,
-                      child: CircleAvatar(
-                        radius: 30.0,
-                        backgroundImage:
-                        NetworkImage(user.picture.toString()),
-                        backgroundColor: Colors.transparent,
+              Container(
+                padding: EdgeInsets.all(5),
+                child: SizedBox(
+                  height: 65.0,
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 10,),
+                      SizedBox(
+                        height: 55,
+                        width: 55,
+                        child: CircleAvatar(
+                          radius: 30.0,
+                          backgroundImage:
+                          NetworkImage(user.picture.toString()),
+                          backgroundColor: Colors.transparent,
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 10,),
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(user.name.toString(), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                          Text(user.nickname.toString(), style: TextStyle(fontSize: 15),)
-                        ],
+                      const SizedBox(width: 10,),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(user.name.toString(), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                            Text(user.nickname.toString(), style: TextStyle(fontSize: 15),)
+                          ],
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        alignment: Alignment.centerRight,
-                        child: const Icon(Icons.cancel),
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.centerRight,
+                          child: IconButton(
+                            onPressed: (){
+                              Navigator.pop(context);
+                            },
+                            icon: Icon(Icons.cancel),
+                          ),
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 10,)
-                  ],
+                      const SizedBox(width: 10,)
+                    ],
+                  ),
                 ),
               ),
-              Expanded(child: Container(
-                  padding: const EdgeInsets.all(15),
-                  child: Text("data.content.toString()",style: const TextStyle(fontSize: 20, color: Colors.black54),),
-                ),
+              TextFormField(
+                initialValue: 'What do you think?',
+                cursorColor: Colors.black,
+                maxLines: 5,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    contentPadding:
+                    EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+                    hintText: "Tell your thinking here"),
               ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height*0.5,
-                child: Center(
+              Expanded(
+                child: Container(
+                  alignment: Alignment.topCenter,
                   child: Image.network(
-                    //data.image.toString(),
                     "https://cdn.baogiaothong.vn/upload/images/2020-4/article_social_image/2020-12-15/noel-1608021530-width1200height630-auto-crop-1608021622-width1200height630-1608021625-width1200height630.jpg",
                     errorBuilder: (context,error,stacktrace){
                       return Container();
@@ -88,10 +106,37 @@ class _UploadStatusState extends State<UploadStatus> {
                   ),
                 ),
               ),
+              Divider(
+                color: Colors.black,
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                      onPressed: (){
+                        Container();
+                      },
+                      icon: Icon(Icons.image),
+                  ),
+                  IconButton(
+                    onPressed: (){
+                      print("1");
+                    },
+                    icon: Icon(Icons.send),
+                  ),
+                ],
+              )
             ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
   }
 }
